8b5af8f5a4d5dd9488e76e9134ac00fdd6a5a570
Noel Morales Jr <82738896+noelmoralesjr@users.noreply.github.com>
2022-04-02 23:18:49 +0000

    Product class was add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import {user} from './user.js'
+import {product} from './product.js'
 
 const ryan = new user('Ryan', 'Yan')
 
 console.log(ryan)
+
+console.log(new product('laptop', 3000))
---
+++ product.js
@@ -0,0 +1,6 @@
+export class product {
+	constructor(name, price) {
+		this.name = name
+		this.price = price	
+	}	
+}
